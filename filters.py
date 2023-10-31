dbb3cf60e3393663277b86938eb546e1e39d407b
Mahdi <74005352+mahdiashtian@users.noreply.github.com>
2023-10-31 16:38:40 +0330

    Add admin_filter

--- filters.py
+++ filters.py
@@ -1,6 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Update
 
+from services import read_user_from_db
+
 
 def conversation(conversation_state: dict, state: str):
     async def func(_, __, update: Update):
@@ -11,4 +13,16 @@ def conversation(conversation_state: dict, state: str):
         name="ConversationStateFilter",
         conversation_state=conversation_state,
         state=state
+    )
+
+
+def admin_filter(db: object):
+    async def func(_, __, update: Update):
+        user = read_user_from_db(_.db, update.from_user.id)
+        return bool(user and (user.is_superuser or user.is_staff))
+
+    return filters.create(
+        func,
+        name="AdminFilter",
+        db=db
     )
